7fc14c33df0eedf428bbb7408543dec13b495e56
TiagoPereira-4i <t.pereira@4intelligence.com.br>
2022-07-11 15:38:49 -0300

    dev created

---
+++ src/functions/calculator.py
@@ -0,0 +1,13 @@
+def sum(a, b):
+    return a+b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("b can not be 0")
+    return a/b
+
+def multiply(a,b):
+    return a*b
+
+def subtract(a, b):
+    return a - b
